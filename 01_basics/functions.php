7e69b922ec08e0634d527aa7074d5f063d4cfa21
orion2403 <orionv2403@gmail.com>
2025-04-14 21:10:20 +0300

    feat(basics): solve tasks

--- 01_basics/functions.php
+++ 01_basics/functions.php
@@ -36,3 +36,64 @@ function maxOfTwo($a, $b)
 {
     return ($a > $b) ? $a : $b;
 }
+
+function updateProfile(&$profile, $name = null, $email = null)
+{
+    if (!empty($name)) {
+        $profile['name'] = $name;
+    }
+
+    if (!empty($email)) {
+        $profile['email'] = $email;
+    }
+}
+
+$profile = [
+    'name' => 'John',
+    'email' => 'john@example.com'
+];
+
+updateProfile($profile, 'Mike');
+
+function validateLogin($username, $minLength = 5)
+{
+    if (empty($username)) {
+        return 'Empty username';
+    }
+    return (mb_strlen($username) < $minLength) ? 'Too short' : 'Valid login';
+}
+
+function atm(&$balance, $amount, $operation)
+{
+    if ($operation == 'withdraw' && $amount > $balance) {
+        return 'Insufficient funds';
+    }
+
+    switch ($operation) {
+        case 'deposit':
+            $balance += $amount;
+            break;
+        case 'withdraw':
+            $balance -= $amount;
+            break;
+        default:
+            return 'Invalid operation';
+    }
+
+    return "Balance updated: $balance";
+}
+
+function checkAge($age, $requiredAge = 18)
+{
+    return ($age < $requiredAge)
+        ? 'Too young'
+        : (($age == $requiredAge) ? 'Just enough' : 'Welcome');
+}
+
+function repeatString($str, $times)
+{
+    if ($times <= 0) {
+        return '';
+    }
+    return $str . repeatString($str, --$times);
+}
