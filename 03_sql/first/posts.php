58afdb8e33dcc3ee6bbdb906e8ffdd949b8232c6
orion2403 <orionv2403@gmail.com>
2025-04-17 17:22:24 +0300

    feat(sql): add rendering all posts on page

---
+++ 03_sql/first/posts.php
@@ -0,0 +1,37 @@
+<table>
+    <tr>
+        <td>ID</td>
+        <td>Title</td>
+        <td>Date</td>
+    </tr>
+    <?php
+    $db = require 'vendor/db.php';
+
+    $posts = mysqli_query($db, 'SELECT * FROM `posts`');
+    $posts = mysqli_fetch_all($posts);
+
+    foreach ($posts as $post) {
+        ?>
+        <tr>
+            <td><?= $post[0] ?></td>
+            <td><?= $post[1] ?></td>
+            <td><?= $post[3] ?></td>
+            <td><a href="vendor/open_post.php?id=<?= $post[0] ?>">Open post</a></td>
+        </tr>
+
+        <?php
+    }
+    ?>
+</table>
+
+<form action="vendor/create.php" method="POST">
+    <label for="name">
+        Title: <br> <input type="text" name="title" id="name">
+    </label>
+    <br>
+    <label for="body">
+        Post body: <br><textarea name="body" id="body"></textarea>
+    </label>
+    <br>
+    <button type="submit">Post</button>
+</form>
