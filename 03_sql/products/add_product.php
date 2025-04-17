698a3b1670495b1503fc476d894ba41185ba9ba9
orion2403 <orionv2403@gmail.com>
2025-04-17 16:59:09 +0300

    feat(http): create simple adding product to database

---
+++ 03_sql/products/add_product.php
@@ -0,0 +1,20 @@
+<?php
+
+$db = mysqli_connect('localhost', 'root', 'yota2016-D', 'hw-db');
+
+if (!$db) {
+    die('Connection to database error');
+}
+
+$product_name = $_POST['name'];
+$description = $_POST['description'];
+$categoryId = $_POST['category_id'];
+$price = $_POST['price'];
+
+$isAdd = mysqli_query($db, "INSERT INTO `products` (`id`, `name`, `description`, `category_id`, `price`) VALUES (NULL, '$product_name', '$description', '$categoryId', '$price')");
+
+if (!$isAdd) {
+    die('Product added failed');
+} else {
+    die('Product added success');
+}
